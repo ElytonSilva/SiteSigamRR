8651dbe91c49304c66efb4f591b3ab09286a2854
Charles Gomes <charles.kratos@gmail.com>
2018-10-10 10:27:19 -0400

    aminações de cartão

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,6 +5,148 @@ img {
   -webkit-filter: drop-shadow(5px 5px 5px #222 );
   filter: drop-shadow(5px 5px 5px #222);
 }
+
+/* Buzz */
+@-webkit-keyframes hvr-buzz {
+  50% {
+    -webkit-transform: translateX(3px) rotate(2deg);
+    transform: translateX(3px) rotate(2deg);
+  }
+  100% {
+    -webkit-transform: translateX(-3px) rotate(-2deg);
+    transform: translateX(-3px) rotate(-2deg);
+  }
+}
+@keyframes hvr-buzz {
+  50% {
+    -webkit-transform: translateX(3px) rotate(2deg);
+    transform: translateX(3px) rotate(2deg);
+  }
+  100% {
+    -webkit-transform: translateX(-3px) rotate(-2deg);
+    transform: translateX(-3px) rotate(-2deg);
+  }
+}
+.hvr-buzz {
+  display: inline-block;
+  vertical-align: middle;
+  -webkit-transform: perspective(1px) translateZ(0);
+  transform: perspective(1px) translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+}
+.hvr-buzz:hover, .hvr-buzz:focus, .hvr-buzz:active {
+  -webkit-animation-name: hvr-buzz;
+  animation-name: hvr-buzz;
+  -webkit-animation-duration: 0.15s;
+  animation-duration: 0.15s;
+  -webkit-animation-timing-function: linear;
+  animation-timing-function: linear;
+  -webkit-animation-iteration-count: infinite;
+  animation-iteration-count: infinite;
+}
+
+/* Buzz Out */
+@-webkit-keyframes hvr-buzz-out {
+  10% {
+    -webkit-transform: translateX(3px) rotate(2deg);
+    transform: translateX(3px) rotate(2deg);
+  }
+  20% {
+    -webkit-transform: translateX(-3px) rotate(-2deg);
+    transform: translateX(-3px) rotate(-2deg);
+  }
+  30% {
+    -webkit-transform: translateX(3px) rotate(2deg);
+    transform: translateX(3px) rotate(2deg);
+  }
+  40% {
+    -webkit-transform: translateX(-3px) rotate(-2deg);
+    transform: translateX(-3px) rotate(-2deg);
+  }
+  50% {
+    -webkit-transform: translateX(2px) rotate(1deg);
+    transform: translateX(2px) rotate(1deg);
+  }
+  60% {
+    -webkit-transform: translateX(-2px) rotate(-1deg);
+    transform: translateX(-2px) rotate(-1deg);
+  }
+  70% {
+    -webkit-transform: translateX(2px) rotate(1deg);
+    transform: translateX(2px) rotate(1deg);
+  }
+  80% {
+    -webkit-transform: translateX(-2px) rotate(-1deg);
+    transform: translateX(-2px) rotate(-1deg);
+  }
+  90% {
+    -webkit-transform: translateX(1px) rotate(0);
+    transform: translateX(1px) rotate(0);
+  }
+  100% {
+    -webkit-transform: translateX(-1px) rotate(0);
+    transform: translateX(-1px) rotate(0);
+  }
+}
+@keyframes hvr-buzz-out {
+  10% {
+    -webkit-transform: translateX(3px) rotate(2deg);
+    transform: translateX(3px) rotate(2deg);
+  }
+  20% {
+    -webkit-transform: translateX(-3px) rotate(-2deg);
+    transform: translateX(-3px) rotate(-2deg);
+  }
+  30% {
+    -webkit-transform: translateX(3px) rotate(2deg);
+    transform: translateX(3px) rotate(2deg);
+  }
+  40% {
+    -webkit-transform: translateX(-3px) rotate(-2deg);
+    transform: translateX(-3px) rotate(-2deg);
+  }
+  50% {
+    -webkit-transform: translateX(2px) rotate(1deg);
+    transform: translateX(2px) rotate(1deg);
+  }
+  60% {
+    -webkit-transform: translateX(-2px) rotate(-1deg);
+    transform: translateX(-2px) rotate(-1deg);
+  }
+  70% {
+    -webkit-transform: translateX(2px) rotate(1deg);
+    transform: translateX(2px) rotate(1deg);
+  }
+  80% {
+    -webkit-transform: translateX(-2px) rotate(-1deg);
+    transform: translateX(-2px) rotate(-1deg);
+  }
+  90% {
+    -webkit-transform: translateX(1px) rotate(0);
+    transform: translateX(1px) rotate(0);
+  }
+  100% {
+    -webkit-transform: translateX(-1px) rotate(0);
+    transform: translateX(-1px) rotate(0);
+  }
+}
+.hvr-buzz-out {
+  display: inline-block;
+  vertical-align: middle;
+  -webkit-transform: perspective(1px) translateZ(0);
+  transform: perspective(1px) translateZ(0);
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+}
+.hvr-buzz-out:hover, .hvr-buzz-out:focus, .hvr-buzz-out:active {
+  -webkit-animation-name: hvr-buzz-out;
+  animation-name: hvr-buzz-out;
+  -webkit-animation-duration: 0.75s;
+  animation-duration: 0.75s;
+  -webkit-animation-timing-function: linear;
+  animation-timing-function: linear;
+  -webkit-animation-iteration-count: 1;
+  animation-iteration-count: 1;
+}
 </style>
 <div class="container">
     <h5 class="center">Painel Administrativo</h5>
@@ -12,7 +154,7 @@ img {
     @include('admin._caminho')
     <div class="row">
         @can('perfil-view')
-        <div class="col s12 m6">
+        <div class="col s12 m6 hvr-buzz-out">
             <div class="card indigo lighten-1">
                 <div class="row">
                     <div class="col s8 m8">
@@ -26,13 +168,12 @@ img {
                     </div>
                     <div class="col s4 m4">
                         <img 
-                        class="img-responsive"
+                        class="responsive-img"
                         src="imagens/icons/user_edit.png"
                         style="
                         position:relative; 
                         float:right; 
-                        margin:10px;
-                        widht:98px; height:98px;">
+                        margin:10px;">
                     </div>
                 </div>
             </div>
@@ -40,7 +181,7 @@ img {
         @endcan
 
         @can('usuario-view')
-        <div class="col s12 m6">
+        <div class="col s12 m6 hvr-buzz-out">
             <div class="card light-blue darken-3">
                 <div class="row">
                     <div class="col s8 m8">
@@ -54,13 +195,12 @@ img {
                     </div>
                     <div class="col s4 m4">
                         <img 
-                        class="img-responsive"
+                        class="responsive-img"
                         src="imagens/icons/user.png"
                         style="
                         position:relative; 
                         float:right; 
-                        margin:10px;
-                        widht:98px; height:98px;">
+                        margin:10px;">
                     </div>
                 </div>
             </div>
@@ -68,7 +208,7 @@ img {
         @endcan
 
         @can('papel-view')
-        <div class="col s12 m6">
+        <div class="col s12 m6 hvr-buzz-out">
             <div class="card cyan darken-3">
                 <div class="row">
                     <div class="col s8 m8">
@@ -82,13 +222,12 @@ img {
                     </div>
                     <div class="col s4 m4">
                         <img 
-                        class="img-responsive"
+                        class="responsive-img"
                         src="imagens/icons/key.png"
                         style="
                         position:relative; 
                         float:right; 
-                        margin:10px;
-                        widht:98px; height:98px;">
+                        margin:10px;">
                     </div>
                 </div>
             </div>
@@ -96,7 +235,7 @@ img {
         @endcan
 
         @can('papel-view')
-        <div class="col s12 m6">
+        <div class="col s12 m6 hvr-buzz-out">
             <div class="card green darken-2">
                 <div class="row">
                     <div class="col s8 m8">
@@ -110,13 +249,12 @@ img {
                     </div>
                     <div class="col s4 m4">
                         <img 
-                        class="img-responsive"
+                        class="responsive-img"
                         src="imagens/icons/confirm.png"
                         style="
                         position:relative; 
                         float:right; 
-                        margin:10px;
-                        widht:98px; height:98px;">
+                        margin:10px;">
                     </div>
                 </div>
             </div>
@@ -124,7 +262,7 @@ img {
         @endcan
         
         @can('papel-view')
-        <div class="col s12 m6">
+        <div class="col s12 m6 hvr-buzz-out">
             <div class="card amber darken-2">
                 <div class="row">
                     <div class="col s8 m8">
@@ -138,13 +276,12 @@ img {
                     </div>
                     <div class="col s4 m4">
                         <img 
-                        class="img-responsive"
+                        class="responsive-img"
                         src="imagens/icons/matricula.png"
                         style="
                         position:relative; 
                         float:right; 
-                        margin:10px;
-                        widht:98px; height:98px;">
+                        margin:10px;">
                     </div>
                 </div>
             </div>
@@ -152,7 +289,7 @@ img {
         @endcan
 
         @can('renovacao-view')
-        <div class="col s12 m6">
+        <div class="col s12 m6 hvr-buzz-out">
             <div class="card deep-orange">
                 <div class="row">
                     <div class="col s8 m8">
@@ -166,13 +303,12 @@ img {
                     </div>
                     <div class="col s4 m4">
                         <img 
-                        class="img-responsive"
+                        class="responsive-img"
                         src="imagens/icons/user_confirm.png"
                         style="
                         position:relative; 
                         float:right; 
-                        margin:10px;
-                        widht:98px; height:98px;">
+                        margin:10px;">
                     </div>
                 </div>
             </div>
@@ -180,7 +316,7 @@ img {
         @endcan
 
 
-        <div class="col s12 m6">
+        <div class="col s12 m6 hvr-buzz-out">
             <div class="card brown darken-1">
                 <div class="row">
                     <div class="col s8 m8">
@@ -194,13 +330,12 @@ img {
                     </div>
                     <div class="col s4 m4">
                         <img 
-                        class="img-responsive"
+                        class="responsive-img"
                         src="imagens/icons/relatorio.png"
                         style="
                         position:relative; 
                         float:right; 
-                        margin:10px;
-                        widht:98px; height:98px;">
+                        margin:10px;">
                     </div>
                 </div>
             </div>
