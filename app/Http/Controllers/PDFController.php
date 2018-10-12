4ff88b398558ebfdd290f07ef275e6b23b55805a
Elyton Silva <elyton_silva18@hotmail.com>
2018-10-12 02:15:43 -0400

    criado controller para gerar pdf

---
+++ app/Http/Controllers/PDFController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\PreMatricula;
+use PDF;
+
+class PDFController extends Controller
+{
+    public function gerarpdf($id){
+        $prematricula = PreMatricula::find($id);
+        
+        $pdf=PDF::loadView('prematricula.confirmacaopdf',['prematricula'=>$prematricula]);
+        return $pdf->stream('Comprovante Pré Matricula.pdf');
+
+    }
+}
