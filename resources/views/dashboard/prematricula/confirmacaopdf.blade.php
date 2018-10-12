0ac2af8fe89aa4f5f8ac0a7ab476653a260f03c0
Elyton Silva <elyton_silva18@hotmail.com>
2018-10-12 02:19:22 -0400

    adicionado um blade teste pdf

---
+++ resources/views/dashboard/prematricula/confirmacaopdf.blade.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+
+<body>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    <html>
+
+    <body>
+        <style>
+            h2 {
+                background-color: #3b3b3b;
+                color: white;
+                font-family: Arial, Helvetica, sans-serif;
+                font-weight: normal;
+                text-align: center;
+            }
+
+            strong {
+                padding-left: 10px;
+
+            }
+
+            strong-2 {
+                padding-left: 280px;
+                background-color: #3b3b3b;
+
+
+            }
+        </style>
+        <div class="conteudo_pagina after" id="conteudo" style="margin: 0 auto; padding-right: 4px;">
+            <div class="mioloFs12 after">
+                <link rel="stylesheet" type="text/css" media="screen, print" href="https://www.ne13.bradesconetempresa.b.br/ibpj/conteudo/css/geral/estrutura.css"
+                />
+                <link rel="stylesheet" type="text/css" media="screen, print" href="https://www.ne13.bradesconetempresa.b.br/ibpj/conteudo/css/geral/interna.css"
+                />
+                <link rel="stylesheet" type="text/css" media="screen, print" href="https://www.ne13.bradesconetempresa.b.br/ibpj/conteudo/css/geral/impressao.css"
+                />
+                <div class="boxComprovante after" style="background: none repeat scroll 0% 0% transparent;">
+                    <div class="after">
+                        <div class="after" style="">
+                            <div class="comprovante">
+                                <table class="tabela_comprovante">
+                                    <tr>
+                                        <td>
+                                            <ul class="cabecalho after">
+                                                <li class="logo transacao">
+                                                    <img src="imagens/IMG-20180314-WA0011.png"
+                                                        class="tabindex tabfirst" id="logoComprovante" />
+                                                </li>
+                                                <li class="info">
+                                                    <h2>Comprovante de Pré-Matrícula:</h </li>
+                                                        <li class="fn clr">
+                                                            <!-- -->
+                                                        </li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div class="comprovante pt15 pb15">
+                                <table class="tabela_comprovante">
+                                    <tr>
+                                        <td>
+                                            <ul class="list_infos after">
+                                                <li class="item">SMEF</li>
+                                                <li class="info">Sistema de gerênciamento de matricula e pré-matricula do Colégio Militar Estadual Dr° Luiz Ritller Brito de Lucena: Endereço: ,,,,,,,</li>
+                                                <li class="quebra">
+                                                    <!-- -->
+                                                </li>
+
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+
+                            <div class="dados comprovante">
+
+                            </div>
+                        </div>
+                        <div class="conteudo_linha pt20 after mt0 bt0">
+                            <p>
+                                <b><font size="4"><strong-2>Dados Cadastrais</strong-2></font></b>
+                            </p>
+                            <br>
+                            <table class="autenticacao tabela_comprovante">
+                                <tr>
+                                    <td>
+                                        <div class="tabelaPre">
+                                            <p>
+                                                <strong>Nome da Mãe: </strong></p>
+                                            <p>
+                                                <strong>Nome do Pai: </strong> </p>
+                                            <p>
+                                                <strong>Nome do Aluno: </strong></p>
+                                            <p>
+                                                <strong>Senha Gerada: </strong> </p>
+
+                                            
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="basesac after">
+                            <div class="sac after">
+
+
+                                <ul class="ouvidoria">
+                                    
+
+                                    <br>
+                                    <br>
+                                    <li class>
+                                    <span>• Xérox do Histórico Escola ou Declaração com notas;</span></br></br>
+                                    <span>• Xérox do RG ou Certidão de Nascimento e CPF;</span></br></br>
+                                    <span>• Xérox do comprovante de residência;</span></br></br>
+                                    <span>• Xérox do cartão de vacina;</span></br></br>
+                                    <span>• 01 Foto 3x4;</span></br></br>
+                                    <span>• Xérox do RG do responsável pelo aluno;</span></br></br>
+                                    <span>• Xérox do Laudo do comprobatório de Defiência do aluno, se for o caso;</span></br></br>
+                                    <span>• Xérox do Cartão do Programa Bolsa Família e RG do titular do cartão;</span></br></br>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+    </body>
+
+    </html>
+</body>
+
+</html>
