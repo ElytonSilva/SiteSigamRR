500c785f6c8bc6b030aa4fda90840dcbbe53b2f1
Elyton Silva <elyton_silva18@hotmail.com>
2018-10-12 19:54:50 -0400

    feito alguns ajuste

--- resources/views/dashboard/relatorios/listaprematricula.blade.php
+++ resources/views/dashboard/relatorios/listaprematricula.blade.php
@@ -12,7 +12,7 @@
                 <input type="text" id="buscar" class="form-control" name="buscar" placeholder="Buscar Nome do Aluno Cadastrado na Pré-Matricula">
                 <div class="col-md-2">
 
-                    <a title="Buscar os código pré-matricula"> <button type= "submit" class="btn btn-default" type="button">Buscar</button></a>
+                    <a title="Buscar os código pré-matricula" button type= "submit" class="btn btn-default" type="button">Buscar</button></a>
 
                 </div>
             </form>
