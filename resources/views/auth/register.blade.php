ca3ef94fbb33c0b38228cd4e0b80809817365d79
Elyton <elyton.moreira163@gmail.com>
2018-10-19 08:57:48 -0400

    REVERT ALTERAÇÕES

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,397 +1,64 @@
+@extends('layouts.app')
 
-<style>
-@import url('https://fonts.googleapis.com/css?family=Poppins');
+@section('content')
+<div class="background-login">
+  <div class="container">
+    <div class="row">
 
-/* BASIC */
-
-html {
-	
-	background-image: url('imagens/slide/blurr-background-green.jpg');
-	width: auto;
-	max-width:100%;
-	background-size:100% 100%;
-	-webkit-background-size: 100% 100%;
-	-o-background-size: 100% 100%;
-	-khtml-background-size: 100% 100%;
-	-moz-background-size: 100% 100%;
-	
-}
-
-body {
-  font-family: "Poppins", sans-serif;
-  height: 100vh;
-}
-
-a {
-  color: #92badd;
-  display:inline-block;
-  text-decoration: none;
-  font-weight: 400;
-}
-
-h2 {
-  text-align: center;
-  font-size: 16px;
-  font-weight: 600;
-  text-transform: uppercase;
-  display:inline-block;
-  margin: 40px 8px 10px 8px; 
-  color: #cccccc;
-}
-
-
-
-/* STRUCTURE */
-
-.wrapper {
-  display: flex;
-  align-items: center;
-  flex-direction: column; 
-  justify-content: center;
-  width: auto;
-  min-height: 100%;
-  padding: 20px;
- 
-  
-}
-
-#formContent {
-  -webkit-border-radius: 10px 10px 10px 10px;
-  border-radius: 10px 10px 10px 10px;
-  background: #fff;
-  padding: 30px;
-   width: auto;
-   height: auto;
-  max-width: 450px;
-  position: relative;
-  padding: 60px;
-  -webkit-box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
-  box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
-  text-align: center;
-  
-}
-
-#formFooter {
-  background-color: #f6f6f6;
-  border-top: 1px solid #dce8f1;
-  padding: 7px;
-  text-align: center;
-  -webkit-border-radius: 0 0 10px 10px;
-  border-radius: 0 0 10px 10px;
-}
-
-
-
-/* TABS */
-
-h2.inactive {
-  color: #cccccc;
-}
-
-h2.active {
-  color: #0d0d0d;
-  border-bottom: 2px solid #5fbae9;
-}
-
-
-
-/* FORM TYPOGRAPHY*/
-
-input[type=button], input[type=submit], input[type=reset]  {
-  background-color: #56baed;
-  border: none;
-  color: white;
-  padding: 15px 80px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  text-transform: uppercase;
-  font-size: 13px;
-  -webkit-box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
-  box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
-  -webkit-border-radius: 5px 5px 5px 5px;
-  border-radius: 5px 5px 5px 5px;
-  margin: 5px 20px 40px 20px;
-  -webkit-transition: all 0.3s ease-in-out;
-  -moz-transition: all 0.3s ease-in-out;
-  -ms-transition: all 0.3s ease-in-out;
-  -o-transition: all 0.3s ease-in-out;
-  transition: all 0.3s ease-in-out;
-}
-
-input[type=button]:hover, input[type=submit]:hover, input[type=reset]:hover  {
-  background-color: #39ace7;
-}
-
-input[type=button]:active, input[type=submit]:active, input[type=reset]:active  {
-  -moz-transform: scale(0.95);
-  -webkit-transform: scale(0.95);
-  -o-transform: scale(0.95);
-  -ms-transform: scale(0.95);
-  transform: scale(0.95);
-}
-
-input[type=text] {
-  background-color: #f6f6f6;
-  border: none;
-  color: #0d0d0d;
-  padding: 15px 20px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 5px;
-  width: auto;
-  border: 1px solid #f6f6f6;
-  -webkit-transition: all 0.5s ease-in-out;
-  -moz-transition: all 0.5s ease-in-out;
-  -ms-transition: all 0.5s ease-in-out;
-  -o-transition: all 0.5s ease-in-out;
-  transition: all 0.5s ease-in-out;
-  -webkit-border-radius: 5px 5px 5px 5px;
-  border-radius: 5px 5px 5px 5px;
-}
-
-input[type=password] {
-  background-color: #f6f6f6;
-  border: none;
-  color: #0d0d0d;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 5px;
-  width: auto;
-  border: 2px solid #f6f6f6;
-  -webkit-transition: all 0.5s ease-in-out;
-  -moz-transition: all 0.5s ease-in-out;
-  -ms-transition: all 0.5s ease-in-out;
-  -o-transition: all 0.5s ease-in-out;
-  transition: all 0.5s ease-in-out;
-  -webkit-border-radius: 5px 5px 5px 5px;
-  border-radius: 5px 5px 5px 5px;
-}
-
-input[type=text]:focus {
-  background-color: #fff;
-  border-bottom: 2px solid #5fbae9;
-}
-
-input[type=password]:placeholder {
-  color: #cccccc;
-}
-
-input[type=password]:focus {
-  background-color: #fff;
-  border-bottom: 2px solid #5fbae9;
-}
-
-input[type=text]:placeholder {
-  color: #cccccc;
-}
-
-
-
-/* ANIMATIONS */
-
-/* Simple CSS3 Fade-in-down Animation */
-.fadeInDown {
-  -webkit-animation-name: fadeInDown;
-  animation-name: fadeInDown;
-  -webkit-animation-duration: 1s;
-  animation-duration: 1s;
-  -webkit-animation-fill-mode: both;
-  animation-fill-mode: both;
- 
-  
-}
-
-@-webkit-keyframes fadeInDown {
-  0% {
-    opacity: 0;
-    -webkit-transform: translate3d(0, -100%, 0);
-    transform: translate3d(0, -100%, 0);
-  }
-  100% {
-    opacity: 1;
-    -webkit-transform: none;
-    transform: none;
-  }
-}
-
-@keyframes fadeInDown {
-  0% {
-    opacity: 0;
-    -webkit-transform: translate3d(0, -100%, 0);
-    transform: translate3d(0, -100%, 0);
-  }
-  100% {
-    opacity: 1;
-    -webkit-transform: none;
-    transform: none;
-  }
-}
-
-/* Simple CSS3 Fade-in Animation */
-@-webkit-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
-@-moz-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
-@keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
-
-.fadeIn {
-  opacity:0;
-  -webkit-animation:fadeIn ease-in 1;
-  -moz-animation:fadeIn ease-in 1;
-  animation:fadeIn ease-in 1;
-
-  -webkit-animation-fill-mode:forwards;
-  -moz-animation-fill-mode:forwards;
-  animation-fill-mode:forwards;
-
-  -webkit-animation-duration:1s;
-  -moz-animation-duration:1s;
-  animation-duration:1s;
-}
-
-.fadeIn.first {
-  -webkit-animation-delay: 0.4s;
-  -moz-animation-delay: 0.4s;
-  animation-delay: 0.4s;
-}
-
-.fadeIn.second {
-  -webkit-animation-delay: 0.6s;
-  -moz-animation-delay: 0.6s;
-  animation-delay: 0.6s;
-}
-
-.fadeIn.third {
-  -webkit-animation-delay: 0.8s;
-  -moz-animation-delay: 0.8s;
-  animation-delay: 0.8s;
-}
-
-.fadeIn.fourth {
-  -webkit-animation-delay: 1s;
-  -moz-animation-delay: 1s;
-  animation-delay: 1s;
-}
-
-/* Simple CSS3 Fade-in Animation */
-.underlineHover:after {
-  display: block;
-  left: 0;
-  bottom: -10px;
-   width: auto;
-  height: 2px;
-  background-color: #56baed;
-  content: "";
-  transition: width 0.2s;
-}
-
-.teste{
-  background-color: #32CD32;
-  border: none;
-  color: white;
-  padding: 15px 80px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  text-transform: uppercase;
-  font-size: 13px;
-  -webkit-box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
-  box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
-  -webkit-border-radius: 5px 5px 5px 5px;
-  border-radius: 5px 5px 5px 5px;
-  margin: 5px 20px 40px 20px;
-  -webkit-transition: all 0.3s ease-in-out;
-  -moz-transition: all 0.3s ease-in-out;
-  -ms-transition: all 0.3s ease-in-out;
-  -o-transition: all 0.3s ease-in-out;
-  transition: all 0.3s ease-in-out;"
-}
-
-.underlineHover:hover {
-  color: #0d0d0d;
-}
-
-.underlineHover:hover:after{
-   width: auto;
-}
-
-
-
-/* OTHERS */
-
-*:focus {
-    outline: none;
-} 
-
-#icon {
-  width:40%;
-}
-
-* {
-  box-sizing: border-box;
-}
-</style>
-<div class="wrapper fadeInDown img-responsive">
-  <div id="formContent">
-    <!-- Tabs Titles -->
-    <h2 class="active"> Criar Conta </h2>
-    
-    <!-- Icon -->
-    <div class="fadeIn first">
-      <img src="imagens/icone-aluno.png" id="icon" alt="User Icon" /><br>
     </div>
+    <div class="row">
+      <h5 class="left">Criar Conta</h4>
+    </div>
+    <div class="row">
+      <div class="card-panel white">
+        <div class="row">
+          <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+            {{ csrf_field() }}
 
-    <!-- Login Form -->
-    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-		{{ csrf_field() }}
-		<div class="input-field col s12">
-		  <input type="text" name="name" value="{{ old('name') }}" id="nome"  class="fadeIn second validate" name="name" placeholder="Nome">
-		    @if ($errors->has('name'))</br>
-            <span>
+            <div class="input-field col s12">
+              <input type="text" name="name" value="{{ old('name') }}" class="validate" autofocus>
+              <label>Nome</label>
+              @if ($errors->has('name'))
+              <span>
                 <strong>{{ $errors->first('name') }}</strong>
-            </span>
-            @endif
-		</div>
-		
-		<div class="input-field col s12">
-		  <input type="text" name="email" value="{{ old('email') }}" id="email"  class="fadeIn second validate" name="login" placeholder="Email">
-			@if ($errors->has('email'))</br>
-				<span>
-					<strong>{{ $errors->first('email') }}</strong>
-				</span>
-			@endif
-		</div>
+              </span>
+              @endif
+            </div>
+            <div class="input-field col s12">
+              <input type="text" name="email" value="{{ old('email') }}" class="validate">
+              <label>E-mail</label>
+              @if ($errors->has('email'))
+              <span>
+                <strong>{{ $errors->first('email') }}</strong>
+              </span>
+              @endif
+            </div>
+            <div class="input-field col s12">
+              <input type="password" name="password" value="{{ old('password') }}" class="validate">
+              <label>Senha</label>
+              @if ($errors->has('password'))
+              <span>
+                <strong>{{ $errors->first('password') }}</strong>
+              </span>
+              @endif
+            </div>
+            <div class="input-field col s12">
+              <input type="password" name="password_confirmation" value="{{ old('password_confirmation') }}" class="validate">
+              <label>Confirme a senha</label>
+              @if ($errors->has('password_confirmation'))
+              <span>
+                <strong>{{ $errors->first('password_confirmation') }}</strong>
+              </span>
+              @endif
+            </div>
+            <button class="btn green">Cadastrar</button>
+          </form>
+        </div>
 
-        <div class="input-field col s12">
-		  <input type="password" name="password" value="{{ old('password') }}" id="password"  class="fadeIn second validate" name="password" placeholder="Senha">
-			@if ($errors->has('password'))</br>
-				<span>
-					<strong>{{ $errors->first('password') }}</strong>
-				</span>
-			@endif
-		</div>
-
-        <div class="input-field col s12">
-		  <input type="password" name="password_confirmation" value="{{ old('password_confirmation') }}" id="password"  class="fadeIn second validate" name="password" placeholder= "Confirmar Senha">
-            @if ($errors->has('password_confirmation'))
-                <span>
-                    <strong>{{ $errors->first('password_confirmation') }}</strong>
-                </span>
-            @endif
-		</div>
-
-		<div class="row">
-		
-	  
-    </form>
-    <div id="formFooter">
-        <input type="submit" class="fadeIn fourth" value="Criar Conta">
+      </div>
     </div>
-   
 
   </div>
 </div>
+
+@endsection
