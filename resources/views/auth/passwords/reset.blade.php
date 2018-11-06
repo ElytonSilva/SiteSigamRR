1493cca3f0c720cc4a218bcd95f6221a85355db4
Elyton Silva <elyton_silva18@hotmail.com>
2018-11-05 23:29:55 -0400

    Função redefinir senhar criada

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,8 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+                <div class = "row"></div><br><br>
+                <div class="panel-heading">Redefinir Senha</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -63,7 +64,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Reset Password
+                                    Redefinir Senha
                                 </button>
                             </div>
                         </div>
